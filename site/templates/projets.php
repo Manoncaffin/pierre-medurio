905dd0fff5c17eeff7d843131d08964c661e49e3
Caffin <caffinmanon@gmail.com>
2025-04-11 12:37:49 +0200

    presta

--- site/templates/projets.php
+++ site/templates/projets.php
@@ -15,6 +15,13 @@
                 </li>
             <?php endforeach ?>
         </ul>
+
+        <?php
+        $images = $page->images()->filterBy('type', 'image');
+        $imageCount = $images->count();
+        ?>
+
+        <p><?= $imageCount ?> image<?= $imageCount > 1 ? 's' : '' ?></p>
     </main>
 </main>
 
