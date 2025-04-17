1990f22a25279ffbe437a486a4289b616555a881
Caffin <caffinmanon@gmail.com>
2025-04-17 15:02:49 +0200

    cgv

---
+++ site/templates/cgv.php
@@ -0,0 +1,64 @@
+<?php snippet('header') ?>
+<link rel="stylesheet" href="<?= url('assets/css/templates/cgv.css') ?>">
+
+<main>
+    <div class="cgv_grid">
+        <div class="col">
+            <div class="intro">
+                <h1 class="title_contact">Conditions générales de vente</h1>
+                <p><?= $page->intro_text()->kti() ?></p>
+            </div>
+            <div class="bloc_1">
+                <h2><?= $page->bloc1_title()->kti() ?></h2>
+                <p><?= $page->bloc1_text()->kti() ?></p>
+            </div>
+            <div class="bloc_2">
+                <h2><?= $page->bloc2_title()->kti() ?></h2>
+                <p><?= $page->bloc2_text()->kti() ?></p>
+            </div>
+            <div class="bloc_3">
+                <h2><?= $page->bloc3_title()->kti() ?></h2>
+                <p><?= $page->bloc3_text()->kti() ?></p>
+            </div>
+            <div class="bloc_4">
+                <h2><?= $page->bloc4_title()->kti() ?></h2>
+                <p><?= $page->bloc4_text()->kti() ?></p>
+            </div>
+            <div class="bloc_5">
+                <h2><?= $page->bloc5_title()->kti() ?></h2>
+                <p><?= $page->bloc5_text()->kti() ?></p>
+            </div>
+            <div class="bloc_6">
+                <h2><?= $page->bloc6_title()->kti() ?></h2>
+                <p><?= $page->bloc6_text()->kti() ?></p>
+            </div>
+            <div class="bloc_7">
+                <h2><?= $page->bloc7_title()->kti() ?></h2>
+                <p><?= $page->bloc7_text()->kti() ?></p>
+            </div>
+            <div class="bloc_8">
+                <h2><?= $page->bloc8_title()->kti() ?></h2>
+                <p><?= $page->bloc8_text()->kti() ?></p>
+            </div>
+            <div class="bloc_9">
+                <h2><?= $page->bloc9_title()->kti() ?></h2>
+                <p> <?= $page->bloc9_text()->kti() ?></p>
+            </div>
+            <div class="bloc_10">
+                <h2><?= $page->bloc10_title()->kti() ?></h2>
+                <p><?= $page->bloc10_text()->kti() ?></p>
+            </div>
+            <div class="bloc_11">
+                <h2><?= $page->bloc11_title()->kti() ?></h2>
+                <p><?= $page->bloc11_text()->kti() ?></p>
+            </div>
+            <div class="bloc_12">
+                <h2><?= $page->bloc12_title()->kti() ?></h2>
+                <p><?= $page->bloc12_text()->kti() ?></p>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php snippet('footer') ?>
--- site/templates/mentions-legales.php
+++ site/templates/mentions-legales.php
@@ -1,8 +1,22 @@
 <?php snippet('header') ?>
+<link rel="stylesheet" href="<?= url('assets/css/templates/mentions-legales.css') ?>">
 
 <main>
-    <h1><?= $page->title() ?></h1>
-    <p><?= $page->text()->kirbytext() ?></p>
+  <div class="legal_notice_grid">
+    <div class="col">
+      <h1 class="title_contact">Mentions légales</h1>
+      <?= $page->column1()->kti() ?>
+      
+      <h2 class="little_title_contact">Propriété intellectuelle</h2>
+      <?= $page->column1_row2()->kt() ?>
+    </div>
+
+    <div class="col">
+      <h2 class="title_contact">Crédits</h2>
+      <?= $page->column2()->kt() ?>
+    </div>
+  </div>
 </main>
+
 
 <?php snippet('footer') ?>
