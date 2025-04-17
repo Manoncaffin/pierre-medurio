fe90b2a2fd68dbccf4c084d097fb78d6a11e4f4c
Caffin <caffinmanon@gmail.com>
2025-04-17 15:05:34 +0200

    home

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,24 +1,23 @@
 <?php snippet('header') ?>
+<link rel="stylesheet" href="<?= url('assets/css/templates/home.css') ?>">
+
 <body>
-<main class="main_home">
-<?php
-$images = $page->images();
+    <main class="main_home">
+        <?php
+        $images = $page->images();
 
-if ($images->count() > 0):
-    $hour = date('G'); // Heure actuelle (0 à 23)
-    $imageIndex = $hour % $images->count();
-    $image = $images->nth($imageIndex);
+        if ($images->count() > 0):
+            $image = $images->shuffle()->first();
 
-    if ($image): ?>
-    <div class="image_home">
-        <img src="<?= $image->url() ?>" alt="Image d’accueil" class="home-image" style="max-width: 100%; height: auto;">
-    <?php else: ?>
-        <p>Image introuvable à l’index <?= $imageIndex ?>.</p>
-    <?php endif; ?>
-    </div>
-<?php else: ?>
-    <p>Aucune image n’est disponible pour la page d’accueil.</p>
-<?php endif; ?>
-</main>
+            if ($image): ?>
+                <div class="image_home">
+                    <img src="<?= $image->url() ?>" alt="Image d’accueil" class="home-image" style="max-width: 100%; height: auto;">
+                </div>
+            <?php else: ?>
+                <p>Image introuvable.</p>
+            <?php endif; ?>
+        <?php else: ?>
+            <p>Aucune image n’est disponible pour la page d’accueil.</p>
+        <?php endif; ?>
+    </main>
 </body>
-
