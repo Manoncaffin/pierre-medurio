dd32c47956f152a97c37d8e1b210b91387de24df
Caffin <caffinmanon@gmail.com>
2025-04-19 19:56:31 +0200

    presta

--- site/templates/prestations.php
+++ site/templates/prestations.php
@@ -3,11 +3,15 @@
 
 <main>
     <section class="portrait">
-        <h2>Portrait</h2>
+        <div class="title_border">
+            <h2>Portrait</h2>
+            <div class="border_portrait"></div>
+        </div>
 
         <?php foreach ($page->portrait()->toStructure() as $item): ?>
             <div class="qa">
                 <h3><?= $item->question() ?></h3>
+                <div class="border_question"></div>
                 <p><?= $item->answer()->kt() ?></p>
             </div>
         <?php endforeach ?>
@@ -18,43 +22,67 @@
                     <img src="<?= $image1->url() ?>" alt="">
                 <?php endif ?>
             </div>
-
             <p><?= $page->photoText1()->text1()->kt() ?></p>
-        </div>
 
-        <div class="image-text">
-        <div class="image-portrait">
-            <?php if ($image2 = $page->image($page->photoText2()->image2()->value())): ?>
-                <img src="<?= $image2->url() ?>" alt="">
-            <?php endif ?>
+            <div class="image-portrait">
+                <?php if ($image2 = $page->image($page->photoText2()->image2()->value())): ?>
+                    <img src="<?= $image2->url() ?>" alt="">
+                <?php endif ?>
             </div>
             <p><?= $page->photoText2()->text2()->kt() ?></p>
         </div>
     </section>
 
     <section class="reportage">
-        <h2>Reportage</h2>
+        <div class="title_border">
+            <h2>Reportage</h2>
+            <div class="border_portrait"></div>
+        </div>
 
         <?php foreach ($page->reportage()->toStructure() as $item): ?>
             <div class="qa">
                 <h3><?= $item->question() ?></h3>
+                <div class="border_question"></div>
                 <p><?= $item->answer()->kt() ?></p>
             </div>
         <?php endforeach ?>
 
-        <div class="image-text">
-            <p><?= $page->photoText3()->text3()->kt() ?></p>
-        </div>
+        <div class="image-text_reportage">
+        <p><?= $page->text3()->kt() ?></p>
 
-        <div class="gallery reportage-gallery">
-            <?php foreach ($page->photoText4()->toFiles() as $image): ?>
-                <img src="<?= $image->url() ?>" alt="">
-            <?php endforeach ?>
+
+            <div class="gallery reportage-gallery">
+                <?php foreach ($page->photoText4()->toFiles() as $image): ?>
+                    <img src="<?= $image->url() ?>" alt="">
+                <?php endforeach ?>
+            </div>
         </div>
     </section>
 
     <section class="atelier">
-        <h2>Atelier</h2>
+        <div class="title_border">
+            <h2>Atelier</h2>
+            <div class="border_portrait"></div>
+        </div>
 
+        <?php foreach ($page->atelier()->toStructure() as $item): ?>
+            <div class="qa">
+                <h3><?= $item->question() ?></h3>
+                <div class="border_question"></div>
+                <p><?= $item->answer()->kt() ?></p>
+            </div>
+        <?php endforeach ?>
 
-        <?php snippet('footer') ?>
+        <div class="image-text_atelier">
+            <p><?= $page->text5()->kt() ?></p>
+
+            <div class="gallery atelier-gallery">
+                <?php foreach ($page->photoText5()->toFiles() as $image): ?>
+                    <img src="<?= $image->url() ?>" alt="">
+                <?php endforeach ?>
+            </div>
+        </div>
+    </section>
+
+    <script src="<?= url('assets/js/answer.js') ?>"></script>
+    <?php snippet('footer') ?>
--- site/templates/projets.php
+++ site/templates/projets.php
@@ -25,28 +25,6 @@ $tags = ['reportage', 'portrait', 'atelier', 'travail personnel', 'tous les proj
         }
     </style>
     <section class="all_projects">
-        <!-- <div class="custom-select-wrapper">
-            <select class="filters-select" id="project-filter">
-                <option value="all">tous les projets</option>
-                <option value="reportage">reportage</option>
-                <option value="portrait">portrait</option>
-                <option value="atelier">atelier</option>
-                <option value="travail-personnel">travail personnel</option>
-            </select>
-        </div>  -->
-
-        <!-- <div class="tags-mobile mobile-dropdown">
-            <input type="checkbox" id="dropdown-toggle" class="dropdown-checkbox">
-            <label for="dropdown-toggle" class="all">tous les projets</label>
-
-            <div class="dropdown-menu">
-                <button class="tag-mobile" data-category="reportage">reportage</button>
-                <button class="tag-mobile" data-category="portrait">portrait</button>
-                <button class="tag-mobile" data-category="atelier">atelier</button>
-                <button class="tag-mobile" data-category="travail-personnel">travail personnel</button>
-                <button class="tag-mobile" data-category="all">tous</button>
-            </div>
-        </div> -->
         <div class="custom-select-wrapper" id="dropdown">
             <input type="hidden" name="projectTags" id="projectTagsInput">
 
